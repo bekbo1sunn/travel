d2a0dc4853e4253a002f02b40379124d4a3404af
Omurbek <odosumbekov@gmail.com>
2023-05-23 17:04:41 +0600

    review

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment, Favorite, Like, Rating
 
-# Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(Favorite)
+admin.site.register(Like)
+admin.site.register(Rating)
